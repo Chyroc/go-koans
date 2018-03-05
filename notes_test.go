0d1fff03649ff09e27d9ab14989ee245aab2deec
Chyroc <chen_yunpeng@foxmail.com>
2018-03-05 13:59:30 +0800

    update

--- notes_test.go
+++ notes_test.go
@@ -7,28 +7,36 @@ import (
 
 func TestNote(t *testing.T) {
 	// basic
-	assert(5^2 == 7)
+	{
+		assert(5^2 == 7)
+		// 乘方是：math.Pow()，这个是按位与
+	}
 
 	// string
-	assert("abc"[0] == uint8('a'))
-	assert(fmt.Sprintf("hello %q", "world") == "hello \"world\"")
-	assert(fmt.Sprintf("your balance: %d and %0.2f", 3, 4.5589) == "your balance: 3 and 4.56")
-
-	// array
-	assert(cap([4]string{"apple", "orange", "mango"}) == 4) // it can hold no more
-	assert(cap([]string{"apple", "orange", "mango"}) == 3)  // it can hold no more
 	{
+		assert("abc"[0] == uint8('a'))
+		assert(fmt.Sprintf("hello %q", "world") == "hello \"world\"")
+		assert(fmt.Sprintf("your balance: %d and %0.2f", 3, 4.5589) == "your balance: 3 and 4.56")
+		// %q 会加上双引号
+		// %0.2f 会四舍五入
+	}
+
+	// array alice
+	{
+		assert(cap([4]string{"apple", "orange", "mango"}) == 4) // it can hold no more
+		assert(cap([]string{"apple", "orange", "mango"}) == 3)  // it can hold no more
+		// array slice在计算cap的不同
+
 		f := []string{"apple", "orange", "mango"}
 		f[0] = "change"
 		assert(f[0] == "change")
-	}
+		// slice会引用计算
 
-	// slice
-	{
 		p := []string{"baby", "baby", "lemon"}
 		assert(cap(p) == 3)
 		p = append(p, "baby!")
 		assert(cap(p) == 6)
+		// append 会扩展一倍
 	}
 
 	// ControlFlow
@@ -41,6 +49,7 @@ func TestNote(t *testing.T) {
 			str = "second"
 		}
 		assert(str == "second")
+		// switch 在没有选项的时候，默认是true
 	}
 
 	// struct
@@ -61,11 +70,13 @@ func TestNote(t *testing.T) {
 			30,
 		}
 		assert(bob == john) // assuredly, bob is certainly not john.. yet
+		// struct只要结构和数据一样，就相同，不需要使用同一个type
 	}
 
 	// Allocation
 	{
 		slice := make([]int, 3)
 		assert(len(slice) == 3) // make() creates slices of a given length
+		// make创建的array的长度是3
 	}
 }
